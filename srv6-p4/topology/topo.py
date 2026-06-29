6981ea494f05b4924a0977f1fcfafc61d48360ff
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-06-28 22:00:06 -0400

    Se asignaron direcciones MAC fijas a los hosts y a cada enlace entre los routers de la topología SRv6. Con este cambio se evita que Mininet genere direcciones MAC aleatorias en cada ejecución, permitiendo que las reglas del plano de control y las capturas de tráfico sean consistentes y reproducibles durante las pruebas.

--- srv6-p4/topology/topo.py
+++ srv6-p4/topology/topo.py
@@ -11,20 +11,7 @@ class P4SwitchBMv2(Switch):
         self.dpid = dpid
 
     def start(self, controllers):
-        device_id = self.name.replace('r', '')
-        grpc_port = 50050 + int(device_id)
-        
-        port_args = ""
-        for idx, intf in enumerate(self.intfList()):
-            if not intf.IP():
-                port_args += f"-i {idx+1}:{intf.name} "
-
-        cmd = (f"simple_switch_grpc --device-id {device_id} {port_args} "
-               f"--thrift-port {9090+int(device_id)} --log-console -- "
-               f"--grpc-server-addr 127.0.0.1:{grpc_port} &")
-        
-        print(f"*** Lanzando BMv2 para {self.name}: {cmd}")
-        self.cmd(cmd)
+        pass
 
     def stop(self):
         self.cmd('killall simple_switch_grpc')
@@ -32,29 +19,64 @@ class P4SwitchBMv2(Switch):
 
 class SRv6Topo(Topo):
     def build(self):
-        h1 = self.addHost('h1', ip='2026::11/64')
-        h2 = self.addHost('h2', ip='2026::42/64')
+        h1 = self.addHost('h1', ip='2026::11/64', mac='00:00:00:00:00:11')
+        h2 = self.addHost('h2', ip='2026::42/64', mac='00:00:00:00:00:42')
 
         r1 = self.addSwitch('r1', cls=P4SwitchBMv2)
         r2 = self.addSwitch('r2', cls=P4SwitchBMv2)
         r3 = self.addSwitch('r3', cls=P4SwitchBMv2)
         r4 = self.addSwitch('r4', cls=P4SwitchBMv2)
 
-        self.addLink(h1, r1)  # r1-eth1 (Puerto 1)
-        self.addLink(r1, r2)  # r1-eth2 (Puerto 2) -> r2-eth1 (Puerto 1)
-        self.addLink(r1, r3)  # r1-eth3 (Puerto 3) -> r3-eth1 (Puerto 1)
-        self.addLink(r2, r4)  # r2-eth2 (Puerto 2) -> r4-eth1 (Puerto 1)
-        self.addLink(r3, r4)  # r3-eth2 (Puerto 2) -> r4-eth2 (Puerto 2)
-        self.addLink(r4, h2)  # r4-eth3 (Puerto 3)
+        self.addLink(
+            h1,
+            r1,
+            addr1='00:00:00:00:00:11',
+            addr2='00:00:00:00:01:01'
+        )
+
+        self.addLink(
+            r1,
+            r2,
+            addr1='00:00:00:00:12:01',
+            addr2='00:00:00:00:12:02'
+        )
+
+        self.addLink(
+            r1,
+            r3,
+            addr1='00:00:00:00:13:01',
+            addr2='00:00:00:00:13:03'
+        )
+
+        self.addLink(
+            r2,
+            r4,
+            addr1='00:00:00:00:24:02',
+            addr2='00:00:00:00:24:04'
+        )
+
+        self.addLink(
+            r3,
+            r4,
+            addr1='00:00:00:00:34:03',
+            addr2='00:00:00:00:34:04'
+        )
+
+        self.addLink(
+            r4,
+            h2,
+            addr1='00:00:00:00:04:42',
+            addr2='00:00:00:00:00:42'
+        )
 
 if __name__ == '__main__':
     setLogLevel('info')
     topo = SRv6Topo()
     net = Mininet(topo=topo, controller=None)
     net.start()
-    
-    print("*** Esperando que levanten los servidores gRPC...")
+
+    print("*** Topologia SRv6 levantada con MAC fijas ***")
     time.sleep(2)
-    
+
     CLI(net)
     net.stop()
